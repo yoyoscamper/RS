4da410ec8cfb75190130b710b6e9988745b9940e
yoyoscamper <530974049@qq.com>
2018-08-13 00:04:52 +0800

    我的设置

---
+++ src/components/setting/memberStore/goodsInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="goodsInfo">
+    <el-row>
+      <el-col :span="10">
+        <div class="block">
+          <el-carousel trigger="click" height="150px">
+            <el-carousel-item v-for="pic in goods.picList" :key="pic.url">
+              <img :src="pic.url"/>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="14">
+
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'goodsInfo',
+      data: function () {
+        return {
+          goods: {
+            picList: [
+              {
+                url: require('../../../assets/mySetting/sp.jpg')
+              },
+              {
+                url: require('../../../assets/mySetting/sp.jpg')
+              }
+            ],
+            name: '商品一',
+            price: 12,
+            sales: 3,
+            status: '已售完'
+          }
+        };
+      }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/setting/memberStore/storeList.vue
+++ src/components/setting/memberStore/storeList.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="storeList">
     <el-row :gutter="20">
-      <el-col :span="6"  class="goodsList" v-for="goods in goodsList" :key="goods.id">
-        <img class="goodsLogo" :src="goods.img"/>
+      <el-col :span="6"  class="goodsList" v-for="goods in goodsList" :key="goods.id" @click="toGoodDetail(goods.id)">
+        <img class="goodsLogo" :src="goods.img" @click="toGoodDetail(goods.id)"/>
         <div>
           <label class="goodsTitle">{{goods.title}}</label>
         </div>
@@ -12,68 +12,78 @@
         </div>
       </el-col>
     </el-row>
+    <goods-info :goodsVisible="goodsVisible"></goods-info>
   </div>
 </template>
 
 <script>
-    export default {
-      name: 'storeList',
-      data: function () {
-        return {
-          goodsList: [
-            {
-              id: '1',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '2',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '3',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '4',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '5',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '6',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            },
-            {
-              id: '7',
-              img: require('../../../assets/mySetting/sp.jpg'),
-              title: '商品一',
-              price: '46.00',
-              sales: 3
-            }
-          ]
-        };
-  }
-    };
+  import goodsInfo from './goodsInfo';
+  export default {
+    name: 'storeList',
+    components: {
+      goodsInfo
+    },
+    data: function () {
+      return {
+        goodsVisible: false,
+        goodsList: [
+          {
+            id: '1',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '2',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '3',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '4',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '5',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '6',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          },
+          {
+            id: '7',
+            img: require('../../../assets/mySetting/sp.jpg'),
+            title: '商品一',
+            price: '46.00',
+            sales: 3
+          }
+        ]
+      };
+    },
+    methods: {
+      toGoodDetail (goodsId) { // 跳转至商品详情
+      }
+    }
+  };
 </script>
 
 <style scoped>
